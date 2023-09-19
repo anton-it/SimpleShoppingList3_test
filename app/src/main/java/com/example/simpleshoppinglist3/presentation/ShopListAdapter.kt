7dd948c4958eea7357b32937d4a64fc9a84dcd03
Admin1 <a89086829981@gmail.com>
2023-09-19 15:48:45 +0300

    Add test ViewHolder

---
+++ app/src/main/java/com/example/simpleshoppinglist3/presentation/ShopListAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.simpleshoppinglist3.presentation
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.simpleshoppinglist3.R
+import com.example.simpleshoppinglist3.domain.ShopItem
+
+class ShopListAdapter: RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>() {
+
+    val list = listOf<ShopItem>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_shop_enabled, parent, false)
+        return ShopItemViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    override fun onBindViewHolder(holder: ShopItemViewHolder, position: Int) {
+        val shopItem = list[position]
+        with(holder) {
+            tvName.text = shopItem.name
+            tvCount.text = shopItem.count.toString()
+            view.setOnLongClickListener {
+                true
+            }
+        }
+    }
+
+    class ShopItemViewHolder(val view: View): RecyclerView.ViewHolder(view) {
+        val tvName = view.findViewById<TextView>(R.id.tv_name)
+        val tvCount = view.findViewById<TextView>(R.id.tv_count)
+
+    }
+}
