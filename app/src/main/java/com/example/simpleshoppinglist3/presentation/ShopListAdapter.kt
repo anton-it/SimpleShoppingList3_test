3ac178dcc35cf7a625d024ecc53f9d56afe34b9c
Admin1 <a89086829981@gmail.com>
2023-09-21 06:57:53 +0300

    Add DiffUtils

--- app/src/main/java/com/example/simpleshoppinglist3/presentation/ShopListAdapter.kt
+++ app/src/main/java/com/example/simpleshoppinglist3/presentation/ShopListAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.simpleshoppinglist3.R
 import com.example.simpleshoppinglist3.domain.ShopItem
@@ -12,8 +13,10 @@ class ShopListAdapter: RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>(
 
     var shopList = listOf<ShopItem>()
         set(value) {
+            val callback = ShopListDiffCallback(shopList, value)
+            val diffResult = DiffUtil.calculateDiff(callback)
+            diffResult.dispatchUpdatesTo(this)
             field = value
-            notifyDataSetChanged()
         }
 
     var onShopItemLongClickListenerL: ((ShopItem) -> Unit)? = null
