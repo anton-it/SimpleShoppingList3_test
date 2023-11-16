4f7a30d1db17dd0aef1a559bf500e8a599681f54
Admin1 <a89086829981@gmail.com>
2023-11-16 11:26:08 +0300

    Translated the files into Russian

--- app/src/main/java/com/example/simpleshoppinglist3/presentation/MainActivity.kt
+++ app/src/main/java/com/example/simpleshoppinglist3/presentation/MainActivity.kt
@@ -68,6 +68,7 @@ class MainActivity : AppCompatActivity(), ShopItemFragment.OnEditingFinishedList
 
             showHelpDialog()
         } else {
+            showHelpDialog()
             return
         }
     }
